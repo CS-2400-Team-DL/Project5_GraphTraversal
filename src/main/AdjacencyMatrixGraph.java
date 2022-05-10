b1513d1d710fe3a98d96fc8124fbfc95800849ea
Dean Mah <dmah34@gmail.com>
2022-05-09 17:10:16 -0700

    Implemented ArrayQueue in AdjacencyMatrixGraph.java

--- src/main/AdjacencyMatrixGraph.java
+++ src/main/AdjacencyMatrixGraph.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
 
 public class AdjacencyMatrixGraph{
     
@@ -403,3 +404,102 @@ final class ArrayStack<T>{
 	
 }
 
+
+final class ArrayQueue<T>{
+    private T[] queue;
+    private int frontIndex;
+    private int backIndex;
+    private static final int DEFAULT_CAPACITY = 3;
+    private static final int MAX_CAPACITY = 10000;
+    public ArrayQueue()
+    {
+        this(DEFAULT_CAPACITY);
+    } 
+    public ArrayQueue(int initialCapacity)
+    {
+        checkCapacity(initialCapacity);
+    
+        
+        @SuppressWarnings("unchecked")
+        T[] tempQueue = (T[]) new Object[initialCapacity + 1];
+        queue = tempQueue;
+        frontIndex = 0;
+        backIndex = initialCapacity;
+    } 
+
+    private boolean checkCapacity(int capacity) {
+        if (capacity > MAX_CAPACITY){
+            return false;
+        }
+        return true;
+    }
+    public void enqueue(T newEntry)
+    {
+    ensureCapacity();
+    backIndex = (backIndex + 1) % queue.length;
+    queue[backIndex] = newEntry;
+    } 
+
+    private void ensureCapacity()
+    {
+        if (frontIndex == ((backIndex + 2) % queue.length)) 
+        {
+            T[] oldQueue = queue;
+            int oldSize = oldQueue.length;
+            int newSize = 2 * oldSize;
+            checkCapacity(newSize);
+         
+            @SuppressWarnings("unchecked")
+            T[] tempQueue = (T[]) new Object[newSize];
+            queue = tempQueue;
+            for (int index = 0; index < oldSize - 1; index++)
+            {
+                queue[index] = oldQueue[frontIndex];
+                frontIndex = (frontIndex + 1) % oldSize;
+            }
+         
+            frontIndex = 0;
+            backIndex = oldSize - 2;
+        }
+    }
+
+    public T dequeue()
+    {
+        if (isEmpty())
+            throw new NoSuchElementException();
+        else
+        {
+            T front = queue[frontIndex];
+            queue[frontIndex] = null;
+            frontIndex = (frontIndex + 1) % queue.length;
+            return front;
+        }
+    }
+
+    public T getFront()
+    {
+        if (isEmpty())
+            throw new NoSuchElementException();
+        else
+            return queue[frontIndex];
+    }
+
+    public boolean isEmpty()
+    {
+        return frontIndex == ((backIndex + 1) % queue.length);
+    }
+
+    public void clear()
+    {
+        if (!isEmpty())
+        {
+            for (int index = frontIndex; index != backIndex; index = (index + 1) % queue.length)
+            {
+                queue[index] = null;
+            }
+            queue[backIndex] = null;
+        }
+        frontIndex = 0;
+        backIndex = queue.length - 1;
+    }
+}
